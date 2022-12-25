44ff424814d22928bdc2e5a3f1e87c33df535f95
Ujjwal R <ujjwal.raj.5798@gmail.com>
2022-12-25 11:14:46 +0530

    removed irrelavant files and updated .gitignore

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-SEARCH_KEY = "AIzaSyD6_svwnad4NrzQ7DBTyGiUxzcy_1VJ3Bc"
-SEARCH_ID = "94ed76e4186f6414f"
+SEARCH_KEY = ""
+SEARCH_ID = ""
 
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl="
 RESULT_COUNT = 10
